e2430f99b47eb47c97d545dbcc03d624696caac4
Danyl Sobolev <info@arraydesign.ca>
2013-08-15 13:35:57 -0400

    initial pass

--- themes/fcf/layouts/signup.php
+++ themes/fcf/layouts/signup.php
@@ -5,10 +5,11 @@
 	<!--[if IE 8]>			<html lang="en" xmlns="http://www.w3.org/1999/xhtml" class="lt-ie9 no-js wf-inactive"> <![endif]-->
 	<!--[if gt IE 8]><!-->	<html lang="en" xmlns="http://www.w3.org/1999/xhtml" class="no-js wf-inactive"> <!--<![endif]-->
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+	<title><?= h($this->page->title) ?> | Fresh City Farms</title>
 	<?= $this->render_partial('layout:head') ?>
 </head>
-<? $active_page = isset($active_page) ? $active_page : 'home' ?>
-<body class="body" id="<?= $active_page ?>">
+<? $active_page = isset($active_page) ? $active_page : 'sign-up-1' ?>
+<body class="body sign-up" id="<?= $active_page ?>">
 <?= $this->render_partial('layout:grid') ?>
 	<div class="content">
 		<?= $this->render_partial('layout:header') ?>
